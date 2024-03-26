cb7f9c65084ab44ee1daa787b43b56ceaa81ae40
benguz <84636906+benguz@users.noreply.github.com>
2024-03-26 18:00:37 -0400

    copy-paste, gradients, css vars

--- src/app/playground/page.js
+++ src/app/playground/page.js
@@ -1,25 +1,26 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import { Flex, Spacer, Box, Table, Thead, Tr, Th, Tbody, Td, Card, CardHeader, Heading, CardBody, Text} from '@chakra-ui/react';
+import { Flex, Input, Spacer, Box, Table, Thead, Tr, Th, Tbody, Td, Card, CardHeader, Heading, CardBody, Text} from '@chakra-ui/react';
 import Image from "next/image";
 import styles from "../page.module.css";
+import CopyPasteElement  from '@/components/copypaste';
 
 export default function Users() {
     return (
         <Flex id="dashboard-container" h="100vh">
             <Flex direction="column" w="55%" padding='1em' id="leftColumn"  spacing="4">
                 <Flex id="topStats" justifyContent="space-between" alignItems="center" margin="10px 0px 10px 0px">
-                    <Card padding="30px">
+                    <Card padding="20px">
                         <CardBody>
                         Example
                         </CardBody>
                     </Card>
-                    <Card padding="30px">
+                    <Card padding="20px">
                         <CardBody>
                         Example
                         </CardBody>
-                    </Card><Card padding="30px">
+                    </Card><Card padding="20px">
                         <CardBody>
                         Example
                         </CardBody>
@@ -34,25 +35,15 @@ export default function Users() {
                         </CardBody>
                     
                 </Card>
-                <Table variant="simple">
-                    <Thead>
-                        <Tr>
-                        <Th>Name</Th>
-                        <Th>Email</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {/* {users.map((user) => (
-                        <Tr key={user.id}>
-                            <Td>{user.name}</Td>
-                            <Td>{user.email}</Td>
-                        </Tr>
-                        ))} */}
-                    </Tbody>
-                    </Table>
+                <CopyPasteElement>
+                    <Text>Testing how the text moves around the input field Testing how the text moves around the input field</Text>
+                    <Text>Hello? <strong>see? bold text!</strong></Text>
+
+                </CopyPasteElement>
+                
             </Flex>
             <Flex direction="column" w='45%' h="100%" padding='1em'  id="RightColumn">
-            <Card variant='elevated' w='100%' h='100%' display='flex' alignItems="center" justify="center">
+            <Card variant='elevated' w='100%' h='80%' backgroundImage='var(--safetykit-gradient)' marginBottom="20px" display='flex' alignItems="center" justify="center">
                 <Image 
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -61,6 +52,12 @@ export default function Users() {
                 height={24}
                 priority/>
               </Card>
+              <Input
+                    variant="outline"
+                    placeholder="Select Date and Time"
+                    size="md"
+                    type="file"
+                />
             </Flex>
             
 
--- src/components/copypaste.js
+++ src/components/copypaste.js
@@ -1,17 +1,73 @@
 import { Box } from '@chakra-ui/react'
-import styled from '@emotion/styled';
+import { useState, useRef } from 'react';
 
-const StyledComponent = styled(Box)`
-//   &:hover {
-//     background: lightgrey;
-//     cursor: pointer;
-//   }
-  width: 55%;
+export default function CopyPasteElement({ children }) {
+    const [isSvgHidden, setIsSvgHidden] = useState(false);
+    const containerRef = useRef(null);
+        
+    function copyText(event) {
+        // Check if the container ref is current and copy its text content
+        if (containerRef.current) {
+            const texts = [];
+            // Iterate over child nodes
+            containerRef.current.childNodes.forEach(node => {
+                // Add the text content of each node to the array
+                texts.push(node.innerText || node.textContent);
+            });
 
-  
-  }
-`;
+            const textToCopy = texts.join('\n');
 
-export default function Responsive({ children }) {
-    return <StyledComponent>{ children }</StyledComponent>
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(textToCopy).then(() => {
+                    console.log('Text copied to clipboard');
+                }).catch(err => {
+                    console.error('Failed to copy text: ', err);
+                });
+            } else {
+                // Fallback for older browsers
+                const textarea = document.createElement('textarea');
+                textarea.value = textToCopy;
+                document.body.appendChild(textarea);
+                textarea.select();
+                document.execCommand('copy');
+                document.body.removeChild(textarea);
+            }
+
+            // Toggle the SVG visibility to indicate a click has happened
+            setIsSvgHidden(true);
+            setTimeout(() => {
+                setIsSvgHidden(false); 
+            }, 150);
+        }
+      }  
+    return <Box className="copy-paste-element">
+        <Box ref={containerRef}>
+        { children }
+        </Box>
+        
+        <button className="copy-icon" onClick={copyText}>
+            <svg className={`svg2 ${isSvgHidden ? 'hidden' : ''}`} viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" width="18" height="18" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <rect x="128" y="128" width="336" height="336" rx="57" ry="57" fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="32"></rect>
+            <path d="M383.5 128l.5-24a56.16 56.16 0 00-56-56H112a64.19 64.19 0 00-64 64v216a56.16 56.16 0 0056 56h24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></path>
+            </svg>
+        </button>
+        </Box>
 }
+
+// SAMPLE USAGE
+{/* <CopyPasteElement>
+  
+    <p>Hi Ms. Harrison,<br>
+    <br>
+I am writing to ask for an extension on the To Kill A Mockingbird essay due Monday. I am behind on this assignment because I have my regional cross country meet on Saturday and have spent an unusual amount of time this week training. 
+    <br><br>
+I can finish the paper by Wednesday morning. I believe this extension would give me the time I need to do my best work. 
+    <br><br>
+Thank you for considering my request, and please let me know if there’s anything else I can do!
+    <br><br>
+Regards,<br>
+April Maddigan  
+</p>
+  </div>
+
+</CopyPasteElement> */}
